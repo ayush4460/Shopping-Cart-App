a7f84302b52d01523d2173c85f227f3cb8a34927
Ayush Zala <ayushgzala@gmail.com>
2023-07-24 14:31:56 +0530

    Cart Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Home from "./components/Home";
+import Header from "./components/Header";
+import "./styles/app.scss"
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
